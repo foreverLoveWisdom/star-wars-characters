fdbb7807ee49ddaab9c4760e9545a78a5c32908a
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-06-20 22:40:57 +0700

    feat: add loading state to CharacterModal image component

--- src/components/CharacterModal.js
+++ src/components/CharacterModal.js
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 const TOTAL_CHARACTERS = 83;
 
 const CharacterModal = ({ character, onClose }) => {
-  const [randomImage, setRandomImage] = useState("");
+  const [randomImage, setRandomImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(true); // Add a loading state
 
   const fetchRandomImage = () => {
     const randomNum = Math.floor(Math.random() * TOTAL_CHARACTERS) + 1;
@@ -21,10 +22,16 @@ const CharacterModal = ({ character, onClose }) => {
           &times;
         </span>
         <h2>{character.name}</h2>
+        <p style={{ display: isLoading ? "block" : "none" }}>
+          Loading image...
+        </p>
         <img
           className="character-image"
           src={randomImage}
           alt={character.name}
+          style={{ display: isLoading ? "none" : "block" }}
+          onLoad={() => setIsLoading(false)} // Set loading to false when image has loaded
+          onError={() => setIsLoading(false)} // Also set loading to false if there was an error loading the image
         />
         <p>Height: {character.height} cm</p>
         <p>Mass: {character.mass} kg</p>
